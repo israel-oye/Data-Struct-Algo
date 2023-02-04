48eb26d017b79d04a72da01f4418a347c5fb64b4
israel-oye <ioyeboade@gmail.com>
2023-02-04 23:18:34 +0100

    Add non-recursive implementation of Binary Search

--- Binary-Search/main.py
+++ Binary-Search/main.py
@@ -19,3 +19,20 @@ def binary_search(array: list, start: int, end: int, target):
         return binary_search(array, mid_index + 1, end, target)
 
 print(binary_search(array=arr, start=start, end=end, target=target))
+
+#Alternative simpler implementation
+#Error prone though as an invalid target could make array go out of bound
+
+def binary_search_2(array: list, target):
+    low = 0
+    high = len(array)
+
+    while low <= high:
+        mid = math.floor((low + high) / 2)
+        if array[mid] == target:
+            return True
+        elif array[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
